1864e379ca7bce3264c1c7bc1d4c933b4d31a2b9
TheDarkLynx786 <murtaza0713@gmail.com>
2024-01-28 06:23:42 -0600

    We try to get this favicon dub

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def title():
 
 st.set_page_config(
         page_title="Dermalyze",
-        page_icon='/images/R.jpg',
+        page_icon="\images\avatar.ico",
         layout="wide",
         initial_sidebar_state="expanded"
     )
